e7612f1b8e9c207fb1f8011c6271c83a7b2bfc02
tranquocdong1 <tranquocdong10.2@gmail.com>
2025-06-23 17:12:26 +0700

    base frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+   import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+   import { ToastContainer } from 'react-toastify';
+   import 'react-toastify/dist/ReactToastify.css';
+   import AdminLoginPage from './pages/AdminLoginPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+   function App() {
+       return (
+           <Router>
+               <ToastContainer />
+               <Routes>
+                   <Route path="/admin/login" element={<AdminLoginPage />} />
+                   <Route path="/" element={<div>Welcome to Online Learning System</div>} />
+               </Routes>
+           </Router>
+       );
+   }
 
-export default App;
+   export default App;
